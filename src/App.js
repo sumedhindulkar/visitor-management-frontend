97afdd99011108ee4d6371e1e926290b9165f39c
Sumedh Indulkar <cscoder2002@gmail.com>
2021-09-25 18:28:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import BuildingForm from "./components/BuildingForm";
+import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BuildingForm />
+      <form>
+        <h2>Form to add new user in a building</h2>
+        <input type="text" placeholder="name" />
+        <input type="number" placeholder="phone number" />
+        <input type="text" placeholder="Address" />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
